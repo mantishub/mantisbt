f2cbab4514d2611becd37b27dae0e4d17c1b9dd0
Walter Gar <walter@mantishub.net>
2020-05-05 16:29:29 -0300

    components index

--- client/components/index.ts
+++ client/components/index.ts
@@ -1,3 +1,4 @@
+export { default as Dropdown } from './Dropdown';
 export { default as DropdownTextInput } from './DropdownTextInput';
 export { default as IssueRelationships } from './IssueRelationships';
 export { default as MentionInput } from './MentionInput';
